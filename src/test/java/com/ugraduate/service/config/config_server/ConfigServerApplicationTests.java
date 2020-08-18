61f8e02e49f94b1f44884d60a7225d334dad4444
FayboCorp <32463786+FayboCorp@users.noreply.github.com>
2020-08-18 07:00:11 -0500

    moved filepath

---
+++ src/test/java/com/ugraduate/service/config/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugraduate.service.config.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
